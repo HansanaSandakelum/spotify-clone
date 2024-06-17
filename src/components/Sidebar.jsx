901a087a221e20e9132e789fe92ce1547d2aa584
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-06-17 20:40:13 +0530

    your library add

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { assets } from "../assets/assets";
 
 const Sidebar = () => {
   return (
-    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
+    <div className="w-[25%] h-full p-2  flex-col gap-2 text-white hidden lg:flex">
       <div className=" bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className=" w-6" src={assets.home_icon} alt="" />
@@ -16,12 +16,16 @@ const Sidebar = () => {
         </div>
       </div>
       <div className=" bg-[#121212] h-[85%] rounded">
-        <div className=" p-4 flex items-center justify-between"></div>
-        <div className=" flex items-center gap-3">
-          <img src={assets.stack_icon} alt="" />
-          <p>Your Library</p>
+        <div className=" p-4 flex items-center justify-between">
+          <div className=" flex items-center gap-3">
+            <img className=" w-8" src={assets.stack_icon} alt="" />
+            <p className=" font-semibold">Your Library</p>
+          </div>
+          <div className=" flex items-center gap-3">
+            <img className="w-5" src={assets.arrow_icon} alt="" />
+            <img className="w-5" src={assets.plus_icon} alt="" />
+          </div>
         </div>
-
       </div>
     </div>
   );
