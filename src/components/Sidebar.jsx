4f0b8d7e8a04c11450e8758ee01b36f8253fdab6
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-06-17 20:09:28 +0530

    app.jsx and components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { assets } from "../assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
+      <div className=" bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className=" w-6" src={assets.home_icon} alt="" />
+          <p className="font-bold">Home</p>
+          {/* <img src={assetsImg.arrow_icon} alt="" /> */}
+        </div>
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className=" w-6" src={assets.search_icon} alt="" />
+          <p className="font-bold">Search</p>
+          {/* <img src={assetsImg.arrow_icon} alt="" /> */}
+        </div>
+      </div>
+      <div className=" bg-[#121212] h-[85%] rounded">
+        <div className=" p-4 flex items-center justify-between"></div>
+        <div className=" flex items-center gap-3">
+          <img src={assets.stack_icon} alt="" />
+          <p>Your Library</p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
